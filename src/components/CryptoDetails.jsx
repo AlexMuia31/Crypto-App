5195d13b2823a785ce3ec6dabe175428ceb20a06
AlexMuia31 <alexmuia31@gmail.com>
2021-11-03 12:51:58 +0300

    bitcoin stats

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -12,7 +12,7 @@ const { Option } = Select;
 
 const CryptoDetails = () => {
     const { coinId } = useParams();
-    const { timePeriod, setTimePeriod } = useState('7d');
+    const [timePeriod, setTimePeriod] = useState('7d');
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
     const cryptoDetails = data?.data?.coin;
 
@@ -37,6 +37,7 @@ const CryptoDetails = () => {
     ];
 
     return (
+
         <Col className='coin-detail-container'>
             <Col className='coin-heading-container'>
                 <Title level={2} className='coin-name'>
@@ -47,7 +48,80 @@ const CryptoDetails = () => {
                     View value statistics, market cap and supply.
                 </p>
             </Col>
+            <Select
+                defaultValue='7d'
+                className='select-timeperiod'
+                placeholder='Select Time Period'
+                onChange={(value) => setTimePeriod(value)}
+            >
+                {time.map((date) => <Option key={date}>{date}</Option>)}
+            </Select>
+            {/* chart */}
+            <Col className='stats-container'>
+                <Col className='coin-value-statistics'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className="coin-details-heading">
+                            {cryptoDetails.name} Value Statistics
+                        </Title>
+                        <p>
+                            An overview showing the stats of {cryptoDetails.name}
+                        </p>
+                    </Col>
+                    {stats.map(({ icon, title, value }) => (
+                        <Col className='coin-stats'>
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className='stats'>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+                <Col className='other-stats-info'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className="coin-details-heading">
+                            Other Statistics
+                        </Title>
+                        <p>
+                            An overview showing the stats of all cryptocurrencies
+                        </p>
+                    </Col>
+                    {genericStats.map(({ icon, title, value }) => (
+                        <Col className='coin-stats'>
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className='stats'>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+            <Col className='coin-desc-link'>
+                <Row className='coin-desc'>
+                    <Title level={3} className='coin-details-heading'>
+                        What is {cryptoDetails.name} ?
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+                </Row>
+                <Col className='coin-links'>
+                    <Title level={3} className='coin-details-heading'>
+                        {cryptoDetails.name} Links
+                    </Title>
+                    {cryptoDetails.links.map((link) => (
+                        <Row className='coin-link' key={link.name}>
+                            <Title level={5} className='link-name'>
+                                {link.type}
+                            </Title>
+                            <a href={link.url} target='_blank' rel='noreferrer'>
+                                {link.name}
+                            </a>
+                        </Row>
+                    ))}
+                </Col>
+            </Col>
         </Col>
+
     )
 }
 
